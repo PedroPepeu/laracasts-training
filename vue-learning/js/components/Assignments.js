cc54dcf5899fbc118f963d4b2ebcd5859c0918f0
PedroPepeu <pedrosteamgtav@gmail.com>
2025-09-29 14:22:56 -0300

    vue-training

---
+++ vue-learning/js/components/Assignments.js
@@ -0,0 +1,43 @@
+import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default {
+    components: { AssignmentList, AssignmentCreate },
+    template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+            <assignment-create></assignment-create>
+        </section>
+    `,
+
+    data() {
+        return {
+            assignments: [
+                { name: 'finish project', complete: false, id: 1 },
+                { name: 'read chapter 4', complete: false, id: 2 },
+                { name: 'turn in homework', complete: false, id: 3 },
+            ],
+        }
+    },
+
+    computed: {
+        filters() {
+            return {
+                inProgress: this.assignments.filter(assignment => !assignment.complete),
+                completed: this.assignments.filter(assignment => assignment.complete),
+            };
+        }
+    },
+
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+        }
+    }
+}
